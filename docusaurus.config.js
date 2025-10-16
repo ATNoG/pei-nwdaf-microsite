690663c00a063ce798c8c55c29b5cd246cb16055
Thiago Vicente <151164926+ThiagoAVicente@users.noreply.github.com>
2025-10-16 15:09:47 +0100

    Update production URL in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: "https://atnog.github.io/pei-nwdaf-microsite",
+  url: "https://atnog.github.io",
   baseUrl: "/",
 
   // GitHub pages deployment config.
